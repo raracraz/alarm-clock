8b31c486cbc764c4a0b4c5c7c656cf3ca12b8236
ryangithub1 <ryan.aaixe@gmail.com>
2021-11-01 18:28:36 +0800

    Update alarmClock.py

--- alarmClock.py
+++ alarmClock.py
@@ -1,5 +1,15 @@
 import datetime
 
-alarmHour = int(input("What hour do you want to wake up?"))
-alarmMinute = int(input("What minute do you want to wake up?"))
-amPm = str(input("Am or Pm"))
+alarmHour = int(input("What hour do you want to wake up?: "))
+alarmMinute = int(input("What minute do you want to wake up?: "))
+amPm = str(input("am or pm"))
+
+if(amPm == "pm"):
+    alarmHour = alarmHour + 12
+
+while True:
+    if(alarmHour == datetime.datetime.now().hour and alarmMinute == datetime.datetime.now().minute):
+        print("Wake up")
+        break
+
+print("exit")
